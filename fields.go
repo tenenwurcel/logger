6333f8d59395cdad02d1e6ca009afbc9f157291e
tenenwurcel <tenenwurcel@pop-os.localdomain>
2023-02-03 12:30:55 -0300

    best result

--- fields.go
+++ fields.go
@@ -21,73 +21,24 @@ var (
 	ErrInvalidType = errors.New("invalid type")
 )
 
-type Field interface {
-	GetKey() string
-	GetType() FieldType
-	GetValue() interface{}
-}
-
-type field struct {
+type Field struct {
 	Key   string
 	Type  FieldType
 	Value interface{}
 }
 
-func (f *field) GetKey() string {
-	return f.Key
-}
-
-func (f *field) GetType() FieldType {
-	return f.Type
-}
-
-func (f *field) GetValue() interface{} {
-	return f.Value
-}
-
-func NewField(key string, fieldType FieldType, value interface{}) (Field, error) {
-	switch fieldType {
-	case StringType:
-		if _, ok := value.(string); !ok {
-			return nil, ErrInvalidType
-		}
-	case BoolType:
-		if _, ok := value.(bool); !ok {
-			return nil, ErrInvalidType
-		}
-	case Float32Type:
-		if _, ok := value.(float32); !ok {
-			return nil, ErrInvalidType
-		}
-	case Float64Type:
-		if _, ok := value.(float64); !ok {
-			return nil, ErrInvalidType
-		}
-	case IntType:
-		if _, ok := value.(int); !ok {
-			return nil, ErrInvalidType
-		}
-	case Int8Type:
-		if _, ok := value.(int8); !ok {
-			return nil, ErrInvalidType
-		}
-	case Int16Type:
-		if _, ok := value.(int16); !ok {
-			return nil, ErrInvalidType
-		}
-	case Int32Type:
-		if _, ok := value.(int32); !ok {
-			return nil, ErrInvalidType
-		}
-	case Int64Type:
-		if _, ok := value.(int64); !ok {
-			return nil, ErrInvalidType
-		}
-	}
-
-	return &field{
+func String(key string, value string) Field {
+	return Field{
 		Key:   key,
-		Type:  fieldType,
+		Type:  StringType,
 		Value: value,
-	}, nil
+	}
+}
+
+func Int(key string, value int) Field {
+	return Field{
+		Key:   key,
+		Type:  IntType,
+		Value: value,
+	}
 }
